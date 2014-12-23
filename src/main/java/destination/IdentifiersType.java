77a8d68224f8c7858e0e2b0596adcae2727df996
Naveen Malik <nmalik@redhat.com>
2014-12-23 08:52:50 -0500

    Added @XmlRootElement to destination.IdentifiersType

--- src/main/java/destination/IdentifiersType.java
+++ src/main/java/destination/IdentifiersType.java
@@ -7,6 +7,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 
 /**
@@ -34,6 +35,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  * 
  */
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "IdentifiersType", propOrder = {
     "masterSystem",
